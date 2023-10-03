208789caacb5dfd36ddf625ac3a2b3ecdfe56a39
Francis <freesix007@gmail.com>
2023-10-03 10:32:16 -0400

    json file petsdata updated

--- PetsLocatorApp/Model/Pet.cs
+++ PetsLocatorApp/Model/Pet.cs
@@ -2,4 +2,11 @@
 
 public class Pet
 {
+    public string Name { get; set; }
+    public string Location { get; set; }
+    public string Details { get; set; }
+    public string Image { get; set; }
+    public string Age { get; set; }
+    public string Latitude { get; set; }
+    public string Longitude { get; set; }
 }
