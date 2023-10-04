5a53bc96bc2a174cf9540462c0a7f8604ba5c557
Francis <freesix007@gmail.com>
2023-10-04 13:00:33 -0400

    Adding HttpClient Service and PetViewModel

---
+++ PetsLocatorApp/Services/IPetService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetsLocatorApp.Services
+{
+    public interface IPetService
+    {
+        Task<IList<Pet>> GetPets();
+    }
+}
---
+++ PetsLocatorApp/Services/PetService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetsLocatorApp.Services
+{
+    public class PetService : IPetService
+    {
+        HttpClient httpClient;
+
+        public PetService()
+        {
+            httpClient = new HttpClient();
+        }
+
+
+        IList<Pet> petList = new List<Pet>();
+
+        public async Task<IList<Pet>> GetPets()
+        {
+            if(petList?.Count > 0)
+                return petList;
+
+            var url = "https://raw.githubusercontent.com/FrancisND/PetsLocatorApp/master/pets-data/petsdata.json";
+            var response = await httpClient.GetAsync(url);
+
+            if(response.IsSuccessStatusCode)
+            {
+                petList = await response.Content.ReadFromJsonAsync<List<Pet>>();
+            }
+
+            //using var stream = await FileSystem.OpenAppPackageFileAsync("petsdata.json");
+            //using var reader = new StreamReader(stream);
+            //var contents = await reader.ReadToEndAsync();
+            //petList = JsonSerializer.Deserialize(contents, PetContext.Default.ListPet);
+
+            return petList;
+        }
+    }
+}
