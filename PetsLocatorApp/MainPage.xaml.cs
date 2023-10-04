69922b9794d9f8d6f24ca7734fd4f767cd11de92
Francis <freesix007@gmail.com>
2023-10-03 20:47:52 -0400

    Updated MainPage

--- PetsLocatorApp/MainPage.xaml.cs
+++ PetsLocatorApp/MainPage.xaml.cs
@@ -2,7 +2,7 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+	//int count = 0;
 
 	public MainPage()
 	{
@@ -11,14 +11,14 @@ public partial class MainPage : ContentPage
 
 	private void OnCounterClicked(object sender, EventArgs e)
 	{
-		count++;
+		//count++;
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+		//if (count == 1)
+		//	CounterBtn.Text = $"Clicked {count} time";
+		//else
+		//	CounterBtn.Text = $"Clicked {count} times";
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
+		//SemanticScreenReader.Announce("You pressed " + CounterBtn.Text + " times");
 	}
 }
 
