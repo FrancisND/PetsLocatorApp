8465e7a37c42c6df08477a664d89b7d2f9c1b93e
Francis <freesix007@gmail.com>
2023-10-07 17:58:49 -0400

    Added Geolocation and Internet Connection Check

--- PetsLocatorApp/ViewModel/PetsViewModel.cs
+++ PetsLocatorApp/ViewModel/PetsViewModel.cs
@@ -11,13 +11,19 @@ namespace PetsLocatorApp.ViewModel
     public partial class PetsViewModel : BaseViewModel
     {
         IPetService petService;
-        
+
+        IConnectivity connectivity;
+        IGeolocation geolocation;
+
         public ObservableCollection<Pet> Pets { get; } = new();
 
-        public PetsViewModel(IPetService _petService)
+
+        public PetsViewModel(IPetService _petService, IConnectivity connectivity, IGeolocation geolocation)
         {
             Title = "Pet Locator";
             petService = _petService;
+            this.connectivity = connectivity;
+            this.geolocation = geolocation;
         }
 
         [RelayCommand]
@@ -28,6 +34,12 @@ namespace PetsLocatorApp.ViewModel
 
             try
             {
+                if (IsNetworkConnectivity())
+                {
+                    await Shell.Current.DisplayAlert("Internet issue!", $"Oops! Check your internet and try again.", "OK");
+                    return;
+                }
+
                 IsBusy = true;
                 var pets = await petService.GetPets();
 
@@ -51,7 +63,7 @@ namespace PetsLocatorApp.ViewModel
         [RelayCommand]
         async Task GoToDetailsAsync(Pet pet)
         {
-            if(pet is null) return;
+            if (pet is null) return;
 
             await Shell.Current.GoToAsync($"{nameof(DetailsPage)}", true,
                 new Dictionary<string, object>
@@ -59,5 +71,54 @@ namespace PetsLocatorApp.ViewModel
                     {"Pet", pet}
                 });
         }
+
+        [RelayCommand]
+        async Task GetClosestPetAsync()
+        {
+            if(IsBusy || !Pets.Any()) return;
+
+            try
+            {
+                var location = await geolocation.GetLastKnownLocationAsync();
+                if(location is null)
+                {
+                    location = await geolocation.GetLocationAsync(
+                        new GeolocationRequest
+                        {
+                            DesiredAccuracy = GeolocationAccuracy.Medium,
+                            Timeout = TimeSpan.FromSeconds(30),
+                        });
+                }
+
+                if (location is null)
+                    return;
+
+                var first = Pets.OrderBy(p => location.CalculateDistance(p.Latitude, p.Longitude, DistanceUnits.Kilometers)).FirstOrDefault();
+
+                if(first is null) return;
+
+                await Shell.Current.DisplayAlert("Closest Pet",
+                    $"{first.Name} in {first.Location}", "OK");
+
+
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                await Shell.Current.DisplayAlert("Error!",
+                    $"Unable to get closest pet : {ex.Message}", "OK");
+            }
+        }
+
+        bool IsNetworkConnectivity() => (connectivity.NetworkAccess != NetworkAccess.Internet);
+        //{
+            //if(connectivity.NetworkAccess != NetworkAccess.Internet)
+            //{
+            //    return false;
+            //}
+
+            //return true;
+        //}
     }
 }
